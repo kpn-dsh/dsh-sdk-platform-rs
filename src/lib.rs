3178626358cc14af4d282f33f051183209986243
frank.hol <frank.hol@kpn.com>
2023-11-30 12:33:33 +0100

    Update API Doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,47 @@
 //! # DSH
 //!
-//! Dsh properties struct. Create new to initialize all related components to connect to the DSH kafka clusters
-//! - Contains a struct similar to datastreams.json
+//! Dsh properties struct. Create new to initialize all related components to connect to the DSH kafka clusters and get metadata of your tenant.
+//! - Avaiablable datastreams info
 //! - Metadata of running container/task
 //! - Certificates for Kafka and DSH
-//!
-//!
-//! ## Local
+//! 
+//! ## High level API
+//! 
+//! The properties struct contains a high level API to interact with the DSH.
+//! This includes generating RDKafka config for creating a consumer/producer and Reqwest config builder for Schema Registry.
+//! 
+//! ### Example:
+//! ```
+//! use dsh_sdk::dsh::Properties;
+//! use rdkafka::consumer::stream_consumer::StreamConsumer;
+//! 
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>>{
+//!     let dsh_properties = Properties::new().await?;
+//!     let consumer: StreamConsumer = dsh_properties.consumer_rdkafka_config().create()?;
+//!     Ok(())
+//! }
+//! ```
+//! 
+//! ### Low level API
+//! It is also possible to get avaiable metadata or the certificates from the properties struct.
+//! 
+//! ### Example:
+//! ```	
+//! use dsh_sdk::dsh::Properties;
+//! use rdkafka::consumer::stream_consumer::StreamConsumer;
+//! 
+//! #[tokio::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>>{
+//!     let dsh_properties = Properties::new().await?;
+//!     // check for write access to topic
+//!     let write_access: bool = dsh_properties.datastream().get_stream("scratch.local.local-tenant").expect("Topic not found").write_access();
+//!     // get the certificates, for example DSH_KAFKA_CERTIFICATE
+//!     let dsh_kafka_certificate = dsh_properties.certificates().map(|certs| certs.dsh_kafka_certificate_pem());
+//!     Ok(())
+//! }
+//! ```
+//! //! ## Local
 //!
 //! It is possible to connect to local kafka cluster by enabling the `local` feature.
 //! This enables to read in the local_datastreams.json file from root folder and parses it into the datastream struct inside the properties struct.
@@ -48,3 +83,4 @@ pub mod dlq;
 mod error;
 #[cfg(feature = "graceful_shutdown")]
 pub mod graceful_shutdown;
+
