f3e6b432c0c9890ddff0852efa597a16651b7ba6
KPNNL\hol504 <frank.hol@kpn.com>
2024-03-05 14:22:51 +0100

    Add get fn and deprecate new plus tests

--- src/dsh/mod.rs
+++ src/dsh/mod.rs
@@ -22,16 +22,19 @@
 //!     Ok(())
 //! }
 //! ```
-use log::warn;
+use log::{debug, info, warn};
+use std::env;
+use std::sync::OnceLock;
 
 use crate::error::DshError;
-use std::env;
 
 pub mod bootstrap;
 pub mod certificates;
 pub mod datastream;
 #[cfg(feature = "local")]
 pub mod local;
+
+static PROPERTIES: OnceLock<Properties> = OnceLock::new();
 
 /// Kafka properties struct. Create new to initialize all related components to connect to the DSH kafka clusters
 ///  - Contains a struct similar to datastreams.json
@@ -57,12 +60,106 @@ pub mod local;
 #[derive(Debug, Clone)]
 pub struct Properties {
     client_id: String,
+    task_id: String,
     tenant_name: String,
     datastream: datastream::Datastream,
     certificates: Option<certificates::Cert>,
 }
 
 impl Properties {
+    /// Get the DSH Properties on a lazy way. If not already initialized, it will initialize the properties
+    /// and bootstrap to DSH.
+    ///
+    /// This struct contains all information and certificates.
+    /// needed to connect to Kafka and DSH.
+    ///
+    ///  - Contains a struct equal to datastreams.json
+    ///  - Metadata of running container/task
+    ///  - Certificates for Kafka and DSH
+    ///
+    /// If running locally, it will try to load the local_datastreams.json file
+    /// and crate the properties struct based on this file
+    ///
+    /// # Error
+    /// If running locally and local_datastreams.json file is not present in the root of the project
+    ///
+    /// # local_datastreams.json
+    /// local_datastreams.json should be placed in the root of the project
+    ///
+    /// Example of local_datastreams.json.
+    /// (important that read and write has correct topic names that are configured in your local kafka cluster)
+    ///
+    /// ```json
+    /// {
+    ///     "brokers": ["localhost:9092"],
+    ///     "streams": {
+    ///       "scratch.local": {
+    ///         "name": "scratch.local",
+    ///         "cluster": "/tt",
+    ///         "read": "scratch.local.local-tenant",
+    ///         "write": "scratch.local.local-tenant",
+    ///         "partitions": 3,
+    ///         "replication": 1,
+    ///         "partitioner": "default-partitioner",
+    ///         "partitioningDepth": 0,
+    ///         "canRetain": false
+    ///       },
+    ///       "stream.test": {
+    ///         "name": "scratch.dlq.local-tenant",
+    ///         "cluster": "/tt",
+    ///         "read": "scratch\\.dlq.\\[^.]*",
+    ///         "write": "scratch.dlq.local-tenant",
+    ///         "partitions": 1,
+    ///         "replication": 1,
+    ///         "partitioner": "default-partitioner",
+    ///         "partitioningDepth": 0,
+    ///         "canRetain": false
+    ///       }
+    ///     },
+    ///     "private_consumer_groups": [
+    ///       "local-app.7e93a513-6556-11eb-841e-f6ab8576620c_1",
+    ///       "local-app.7e93a513-6556-11eb-841e-f6ab8576620c_2",
+    ///       "local-app.7e93a513-6556-11eb-841e-f6ab8576620c_3",
+    ///       "local-app.7e93a513-6556-11eb-841e-f6ab8576620c_4"
+    ///     ],
+    ///     "shared_consumer_groups": [
+    ///       "local-app_1",
+    ///       "local-app_2",
+    ///       "local-app_3",
+    ///       "local-app_4"
+    ///     ],
+    ///     "non_enveloped_streams": [],
+    ///     "schema_store": "http://localhost:8081/apis/ccompat/v7"
+    ///   }
+    /// ```
+    pub async fn get() -> Result<&'static Self, DshError> {
+        //let properties = PROPERTIES.get_or_init(Self::init)
+        if let Some(properties) = PROPERTIES.get() {
+            debug!("Properties already initialized");
+            Ok(properties)
+        } else {
+            let properties: Properties = Self::init().await?;
+            match PROPERTIES.set(properties) {
+                Ok(_) => info!("Properties initialized"),
+                Err(_) => warn!("Properties already initialized by different thread. Ignoring."),
+            }
+            Ok(PROPERTIES.get().unwrap())
+        }
+    }
+    async fn init() -> Result<Self, DshError> {
+        #[cfg(not(feature = "local"))]
+        let result = Self::new_dsh().await;
+        #[cfg(feature = "local")]
+        let result = match Self::new_dsh().await {
+            Ok(b) => Ok(b),
+            Err(e) => {
+                warn!("App does not seem to be running on DSH, due to: {}", e);
+                warn!("Starting local properties instead");
+                Self::new_local()
+            }
+        };
+        result
+    }
     /// Create a new Properties struct that contains all information and certificates.
     /// needed to connect to Kafka and DSH.
     ///
@@ -82,7 +179,7 @@ impl Properties {
     /// Example of local_datastreams.json.
     /// (important that read and write has correct topic names that are configured in your local kafka cluster)
     ///
-    /// ```text
+    /// ```json
     /// {
     ///     "brokers": ["localhost:9092"],
     ///     "streams": {
@@ -125,6 +222,10 @@ impl Properties {
     ///     "schema_store": "http://localhost:8081/apis/ccompat/v7"
     ///   }
     /// ```
+    #[deprecated(
+        since = "0.2.0",
+        note = "Use get() instead to avoid multiple bootstraps to DSH. Function to be removed in 0.3.0"
+    )]
     pub async fn new() -> Result<Self, DshError> {
         #[cfg(not(feature = "local"))]
         let result = Self::new_dsh().await;
@@ -139,7 +240,6 @@ impl Properties {
         };
         result
     }
-
     /// Create a new Properties on a blocking struct that contains all information and certificates.
     /// needed to connect to Kafka and DSH.
     ///
@@ -159,7 +259,7 @@ impl Properties {
     /// Example of local_datastreams.json.
     /// (important that read and write has correct topic names that are configured in your local kafka cluster)
     ///
-    /// ```text
+    /// ```json
     /// {
     ///     "brokers": ["localhost:9092"],
     ///     "streams": {
@@ -202,11 +302,16 @@ impl Properties {
     ///     "schema_store": "http://localhost:8081/apis/ccompat/v7"
     ///   }
     /// ```
+    #[deprecated(
+        since = "0.2.0",
+        note = "Use get_blocking() instead to avoid multiple bootstraps to DSH. Function to be removed in 0.3.0"
+    )]
     pub fn new_blocking() -> Result<Self, DshError> {
-        tokio::runtime::Builder::new_current_thread()
+        let properties = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .build()?
-            .block_on(Self::new())
+            .block_on(Self::get());
+        properties.map(|p| p.clone())
     }
 
     /// Get default RDKafka Consumer config to connect to Kafka on DSH.
@@ -252,7 +357,7 @@ impl Properties {
     pub fn consumer_rdkafka_config(&self) -> Result<rdkafka::config::ClientConfig, DshError> {
         let mut config = rdkafka::config::ClientConfig::new();
         config
-            .set("bootstrap.servers", self.datastream().get_brokers())
+            .set("bootstrap.servers", self.datastream().get_brokers_string())
             .set(
                 "group.id",
                 self.datastream()
@@ -318,7 +423,7 @@ impl Properties {
     pub fn producer_rdkafka_config(&self) -> Result<rdkafka::config::ClientConfig, DshError> {
         let mut config = rdkafka::config::ClientConfig::new();
         config
-            .set("bootstrap.servers", self.datastream().get_brokers())
+            .set("bootstrap.servers", self.datastream().get_brokers_string())
             .set("client.id", self.client_id())
             .set_log_level(rdkafka::config::RDKafkaLogLevel::Info);
 
@@ -377,9 +482,14 @@ impl Properties {
         &self.client_id
     }
 
-    /// Get the tenant name.
+    /// Get the tenant name of running container.
     pub fn tenant_name(&self) -> &str {
         &self.tenant_name
+    }
+
+    /// Get the task id of running container.
+    pub fn task_id(&self) -> &str {
+        &self.task_id
     }
 
     /// Get the kafka properties provided by DSH (datastreams.json)
@@ -410,6 +520,16 @@ pub fn get_configured_topics() -> Result<Vec<String>, DshError> {
 mod tests {
     use super::*;
 
+    fn test_properties() -> Properties {
+        Properties {
+            client_id: "test_client_id".to_string(),
+            task_id: "test_task_id".to_string(),
+            tenant_name: "test".to_string(),
+            datastream: datastream::Datastream::default(),
+            certificates: None,
+        }
+    }
+
     #[test]
     fn test_get_configured_topics() {
         std::env::set_var("TOPICS", "topic1, topic2, topic3");
@@ -428,14 +548,98 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn test_get_if_set() {
+
+        PROPERTIES.set(test_properties()).unwrap();
+        let properties = Properties::get().await;
+        assert!(properties.is_ok());
+        let properties = properties.unwrap();
+        assert_eq!(properties.client_id(), "test_client_id");
+        assert_eq!(properties.task_id(), "test_task_id");
+        assert_eq!(properties.tenant_name(), "test");
+    }
+
+    #[allow(deprecated)]
+    #[tokio::test]
     async fn test_new() {
         let properties = Properties::new().await;
         assert!(properties.is_ok());
     }
 
+    #[allow(deprecated)]
     #[test]
     fn test_new_blocking() {
         let properties = Properties::new_blocking();
         assert!(properties.is_ok());
+    }
+
+    #[test]
+    fn test_consumer_rdkafka_config() {
+        let properties = &test_properties();
+        let config = properties.consumer_rdkafka_config();
+        assert!(config.is_ok());
+        let config = config.unwrap();
+        assert_eq!(
+            config.get("bootstrap.servers").unwrap(),
+            properties.datastream().get_brokers_string()
+        );
+        assert_eq!(
+            config.get("group.id").unwrap(),
+            properties
+                .datastream()
+                .get_group_id(datastream::GroupType::from_env())
+                .unwrap()
+        );
+        assert_eq!(config.get("client.id").unwrap(), properties.client_id());
+        assert_eq!(config.get("enable.auto.commit").unwrap(), "false");
+        assert_eq!(config.get("enable.auto.offset.store").unwrap(), "false");
+        assert_eq!(config.get("auto.offset.reset").unwrap(), "earliest");
+    }
+
+    #[tokio::test]
+    async fn test_producer_rdkafka_config() {
+        let properties = &test_properties();
+        let config = properties.producer_rdkafka_config();
+        assert!(config.is_ok());
+        let config = config.unwrap();
+        assert_eq!(
+            config.get("bootstrap.servers").unwrap(),
+            properties.datastream().get_brokers_string()
+        );
+        assert_eq!(config.get("client.id").unwrap(), properties.client_id());
+    }
+
+    #[test]
+    fn test_reqwest_client_config() {
+        let properties = &test_properties();
+        let config = properties.reqwest_client_config();
+        assert!(config.is_ok());
+    }
+
+    #[test]
+    fn test_client_id() {
+        let properties = &test_properties();
+        assert_eq!(properties.client_id(), "local_client_id");
+    }
+
+    #[test]
+    fn test_tenant_name() {
+        let properties = &test_properties();
+        assert_eq!(properties.tenant_name(), "local");
+    }
+
+    #[test]
+    fn test_task_id() {
+        let properties = &test_properties();
+        assert_eq!(properties.task_id(), "local_task_id");
+    }
+
+    #[test]
+    fn test_schema_registry_host() {
+        let properties = &test_properties();
+        assert_eq!(
+            properties.schema_registry_host(),
+            "http://localhost:8081/apis/ccompat/v7"
+        );
     }
 }
