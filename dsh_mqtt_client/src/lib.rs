05164d3f1e54c564ef9529cd125448d4b836556c
isik508 <s.isik02@kpn.com>
2024-06-24 11:01:25 +0200

    removed examples from lib.rs temporarily

--- dsh_mqtt_client/src/lib.rs
+++ dsh_mqtt_client/src/lib.rs
@@ -8,14 +8,6 @@
 //!
 //! DshConfig is to prepare URLs for Rest and MQTT token api depending on DSH environment. Create DshConfig with only passing DshEnv.
 //!
-//! ### Example:
-//! ```
-//! use std::sync::Arc;
-//! use dsh_mqtt_client::{config::{DshConfig, DshEnv}};
-//!
-//! let dsh_conf = Arc::new(DshConfig::new(DshEnv::Dev));
-//! ```
-//!
 //! # Model
 //!
 //! RetrieveTokenRequest is the model user should use to pass tenant, api_key, claims and client_id information. Suggested way of setting tenant and api_key is setting via environment variables.
@@ -24,29 +16,7 @@
 //!
 //! Create `DshAuthenticationServiceAdapter` and call `retrieve_token`` function passing `RetrieveTokenRequest` you created.
 //!
-//! ### Example:
-//! ```
-//! use std::sync::Arc;
-//! use std::env;
-//! use dsh_mqtt_client::{config::{DshConfig, DshEnv}};
-//! use dsh_mqtt_client::service::{DshAuthenticationServiceAdapter,AuthenticationService};
-//! use dsh_mqtt_client::model::mqtt_model::MqttToken;
-//! use dsh_mqtt_client::model::token_request_attr::RetrieveTokenRequest;
-//!
-//! #[tokio::main]
-//! # async fn main() {
-//! let retrieve_request = RetrieveTokenRequest {
-//! tenant: env::var("TENANT_NAME").unwrap().to_string(),
-//! api_key: env::var("API_KEY").unwrap().to_string(),
-//! claims: None,
-//! client_id: uuid::Uuid::new_v4().to_string(),
-//! };
-//!
-//! let dsh_conf = Arc::new(DshConfig::new(DshEnv::Dev));
-//! let service: DshAuthenticationServiceAdapter = DshAuthenticationServiceAdapter::new(dsh_conf);
-//! let mqtt_token: MqttToken = service.retrieve_token(retrieve_request.clone()).await.unwrap();
-//! }
-//! ```
+
 /// Includes all the logic and steps to fetch rest and mqtt tokens.
 mod authentication_client;
 /// Prepares api endpoints depending on DSH environment.
