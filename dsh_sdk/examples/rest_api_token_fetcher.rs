17a2a4c9879af8d6487baf3f2411681143c650e3
Frank Hol <frank.hol@hotmail.com>
2024-06-27 15:49:25 +0200

    Clean up example

--- dsh_sdk/examples/rest_api_token_fetcher.rs
+++ dsh_sdk/examples/rest_api_token_fetcher.rs
@@ -1,6 +1,5 @@
 use dsh_rest_api_client::Client;
-use dsh_sdk::Platform;
-use dsh_sdk::RestTokenFetcherBuilder;
+use dsh_sdk::{Platform, RestTokenFetcherBuilder};
 
 const CLIENT_SECRET: &str = "";
 const TENANT: &str = "tenant-name";
@@ -8,7 +7,6 @@ const TENANT: &str = "tenant-name";
 #[tokio::main]
 async fn main() {
     let platform = Platform::NpLz;
-
     let client = Client::new(platform.endpoint_rest_api());
 
     let tf = RestTokenFetcherBuilder::new(platform)
